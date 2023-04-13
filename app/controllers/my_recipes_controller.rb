ffee56e7848c466ca3eeba3546c67156389850da
Tim <tim.andrejc02@gmail.com>
2023-04-13 09:35:42 +0200

    my recipes

--- app/controllers/my_recipes_controller.rb
+++ app/controllers/my_recipes_controller.rb
@@ -1,5 +1,6 @@
 class MyRecipesController < ApplicationController
     def index
-        @recipes = Recipe.where(user_id: current_user.id)
+        @myRecipes = Recipe.where(user_id: current_user.id, confirmed: true)
+        @myRecipesNotPosted = Recipe.where(user_id: current_user.id, confirmed: nil)
     end
 end
