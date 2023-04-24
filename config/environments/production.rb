c8e1df006c2294f29b691f4e87989c85efe96905
Aneja <91323255+AnejaPirnat@users.noreply.github.com>
2023-04-24 12:29:10 +0200

    idk

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,7 +24,8 @@ Rails.application.configure do
 
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
-  config.action_controller.perform_caching = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { host: 'yummyai.herokuapp.com' }
 
