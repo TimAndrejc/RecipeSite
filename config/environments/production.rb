86b8fc5e91efb59516e087bcc61f9f05f4235a35
Aneja <91323255+AnejaPirnat@users.noreply.github.com>
2023-04-24 12:57:27 +0200

    port check

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,7 +21,7 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'yummyai.herokuapp.com' }
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
-    port:                 465,
+    port:                 587,
     domain:               'anejapirnat.eu',
     user_name:            ENV['SENDGRID_USERNAME'],
     password:             ENV['SENDGRID_PASSWORD'],
