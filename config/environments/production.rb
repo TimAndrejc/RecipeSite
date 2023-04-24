968d3b11a70c8b986141e814312316330a41082e
Aneja <91323255+AnejaPirnat@users.noreply.github.com>
2023-04-24 13:02:41 +0200

    prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,7 +20,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { host: 'yummyai.herokuapp.com' }
   config.action_mailer.smtp_settings = {
-    address:              'smtp.gmail.com',
+    address:              'mail.anejapirnat.eu',
     port:                 465,
     domain:               'anejapirnat.eu',
     user_name:            ENV['SENDGRID_USERNAME'],
